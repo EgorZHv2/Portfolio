7a73e8db8077124666b00ec290c0e131d7751e22
Егор <egor.zholobov@yandex.ru>
2021-12-04 11:48:43 +0300

    Add project files.

---
+++ Portfolio/Pages/LoginPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Portfolio.Pages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationPage registrationPage = new RegistrationPage();
+            NavigationService.Navigate(registrationPage);
+        }
+
+        private void BTLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LoginPage loginPage = new LoginPage();
+            NavigationService.Navigate(loginPage);
+            
+        }
+        private void BTMenu_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button bt = sender as Button;
+            bt.Background = new SolidColorBrush(Color.FromRgb(66, 106, 30));
+
+        }
+
+        private void BTMenu_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button bt = sender as Button;
+            bt.Background = new SolidColorBrush(Color.FromRgb(100, 161, 46));
+        }
+    }
+}
